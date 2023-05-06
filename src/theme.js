ce590e99562ca22c4c75d34be4bf6c9979c6481c
James Langley <jwblangley@gmail.com>
2023-05-06 17:09:02 +0100

    Create react app

---
+++ src/theme.js
@@ -0,0 +1,31 @@
+import { createTheme } from '@mui/material/styles';
+
+
+export default createTheme({
+    palette: {
+        primary: {
+            light: '#7d87df',
+            dark: '#0d317d',
+            main: '#4b5aad',
+            contrastText: '#ffffff'
+        },
+        secondary: {
+            light: '#ffb66e',
+            dark: '#dd6e00',
+            main: '#ff9b38',
+            contrastText: '#ffffff'
+        },
+        success: {
+            light: '#00f563',
+            dark: '#007E33',
+            main: '#00C851',
+            contrastText: '#ffffff'
+        },
+        error: {
+            light: '#ff8888',
+            dark: '#CC0000',
+            main: '#ff4444',
+            contrastText: '#ffffff'
+        }
+    }
+})
