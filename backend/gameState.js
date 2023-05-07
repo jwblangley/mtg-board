94e82aa4e1fb817f7e205828f2804af0040d81c2
James Langley <jwblangley@gmail.com>
2023-05-07 10:34:13 +0100

    Complete state updates

--- backend/gameState.js
+++ backend/gameState.js
@@ -3,20 +3,36 @@ function generateEmptyBattlefield(width, height) {
 }
 
 class GameState {
-    constructor(battlefieldWidth, battledieldHeight) {
+    constructor(publishStateUpdate, battlefieldWidth, battledieldHeight) {
+        this.publishStateUpdate = publishStateUpdate
         // this.battlefield = generateEmptyBattlefield(battlefieldWidth, battledieldHeight)
 
         // Set up test content
         const testContent = generateEmptyBattlefield(battlefieldWidth, battledieldHeight)
         testContent[0][0] = [
+            { id: "a" },
+            { id: "b" },
+            { id: "c" },
+            { id: "d" },
+            { id: "e" }
+        ]
+
+        this.battlefield = testContent
+
+        // Arbitrary state updates
+        this.counter = 0
+        setInterval(() => {
+            const testContent = generateEmptyBattlefield(battlefieldWidth, battledieldHeight)
+            testContent[0][0] = [
                 { id: "a" },
                 { id: "b" },
                 { id: "c" },
                 { id: "d" },
                 { id: "e" }
-            ]
-
-        this.battlefield = testContent
+            ].slice(this.counter % 5)
+            this.counter += 1
+            this.publishStateUpdate(testContent)
+        }, 500)
     }
 
     getState() {
--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,20 @@ const io = new Server({
     }
 });
 
-let gameState = new GameState(2,2)
+const MESSAGE_TYPES = {
+    GAMESTATE: "gameState"
+}
+
+function publishStateUpdate(gameState) {
+    io.emit(MESSAGE_TYPES.GAMESTATE, gameState)
+}
+
+let gameState = new GameState(publishStateUpdate, 2,2)
 
 io.on("connection", (socket => {
     console.log("A user connected")
     const userId = socket.handshake["query"]["userId"]
-    socket.emit("gameState", gameState.getState())
+    socket.emit(MESSAGE_TYPES.GAMESTATE, gameState.getState())
 }))
 
 io.on("disconnect", (socket) => {
