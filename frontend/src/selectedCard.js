8a70757ad72c91982e8703f1b95b1b67d95e690b
James Langley <jwblangley@gmail.com>
2023-10-21 14:55:17 +0100

    Implement empty card

--- frontend/src/selectedCard.js
+++ frontend/src/selectedCard.js
@@ -1,20 +1,30 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const SERVER_ADDRESS = process.env["REACT_APP_SERVER_ADDRESS"]
 
-const EMPTY_IMG = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs="
-
-
 const SelectedCard = ({ card }) => {
     return (
         <div className="selectedCard">
-            <img
-                alt="Card Preview"
-                src={!!card?.image ? `${SERVER_ADDRESS}/card-image/${card.image}` : EMPTY_IMG}
-                style={{
-                    width: "100%",
-                }}
-            />
+            {
+                !!card?.image ? (
+                    <img
+                        alt="Card Preview"
+                        src={`${SERVER_ADDRESS}/card-image/${card.image}`}
+                        style={{
+                            width: "100%",
+                        }}
+                    />
+                ) : (
+                    <div className="emptyCard">
+                        <Typography
+                            variant="h5"
+                        >
+                            No card selected
+                        </Typography>
+                    </div>
+                )
+            }
         </div>
     )
 }
